7760c49e0c50287b73cb554e369b431933adea2c
XeoNovaDan <toghill7252010@hotmail.co.uk>
2019-08-31 15:29:34 +0100

    Can't remember tbh

---
+++ Source/TargetingModes/HarmonyPatches/CombatTweaks/Patch_DamageWorker_Bite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace TargetingModes
+{
+
+    public static class Patch_CombatTweaks_CombatTweaksUtility
+    {
+
+        public static class ManualPatch_AccuracyScore_AimOnTargetChance_StandardTarget
+        {
+
+            public static void Postfix(ShotReport report, ref float __result)
+            {
+                var traverse = Traverse.Create(report);
+                var pawn = traverse.Field("");
+            }
+
+        }
+
+    }
+
+}
---
+++ Source/TargetingModes/ModCompatibilityCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace TargetingModes
+{
+
+    public static class ModCompatibilityCheck
+    {
+
+        public static bool CombatTweaks => ModsConfig.ActiveModsInLoadOrder.Any(m => m.Name == "[XND] Combat Tweaks");
+
+    }
+
+}
